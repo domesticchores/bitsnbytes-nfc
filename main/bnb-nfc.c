9d6a85026dc2cb160cf75988f7aeb1e59dcc69a4
Akash Keshav <gameplaywithak@gmail.com>
2025-10-28 23:29:32 -0400

    begin SPI integration

--- main/bnb-nfc.c
+++ main/bnb-nfc.c
@@ -6,6 +6,16 @@
 #include <stdlib.h>
 #include <esp_log.h>
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/ioctl.h>
+
+#include "ext_vfs.h"
+#include "ioctl/esp_spi_ioctl.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -24,8 +34,42 @@
 #define RESET_PIN  (-1)
 #define IRQ_PIN    (-1)
 
+#define SPI_DEVICE_CLOCK        (1000000)
+#define SPI_DEVICE_CS_PIN       (5)
+#define SPI_DEVICE_SCLK_PIN     (18)
+#define SPI_DEVICE_MOSI_PIN     (23)
+#define SPI_DEVICE_MISO_PIN     (19)
+
+#define SPI_RECV_BUF_SIZE       (32)
+
+#define SPI_DEVICE "/dev/spi/2"
+
 
 static const char *TAG = "bnb_nfc";
+
+void run_nfc(pn532_io_t pn532_io,esp_err_t err) {
+    uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0}; // Buffer to store the returned UID
+    uint8_t uid_length;                     // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    int repeater = 0;
+
+    if (repeater > 0) {
+            ESP_LOG_BUFFER_HEX_LEVEL(TAG, uid, uid_length, ESP_LOG_INFO);
+            repeater--;
+        }
+
+        // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+        err = pn532_read_passive_target_id(&pn532_io, PN532_BRTY_ISO14443A_106KBPS, uid, &uid_length, 1000);
+
+        if (ESP_OK == err)
+        {
+            ESP_LOG_BUFFER_HEX_LEVEL(TAG, uid, uid_length, ESP_LOG_INFO);
+            repeater = 10;
+
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        } else {
+            ESP_LOGI(TAG,"NULL");
+        }
+}
 
 void app_main()
 {
@@ -33,8 +77,6 @@ void app_main()
     esp_err_t err;
 
     ESP_LOGI("INIT", "Loading bitsnbytes NFC for PN532");
-
-    
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 
@@ -67,70 +109,54 @@ void app_main()
     ESP_LOGI(TAG, "Found chip PN5%x", (unsigned int)(version_data >> 24) & 0xFF);
     ESP_LOGI(TAG, "Firmware ver. %d.%d", (int)(version_data >> 16) & 0xFF, (int)(version_data >> 8) & 0xFF);
 
+    // init SPI
+       int fd;
+    int ret;
+    spi_cfg_t cfg;
+    const char device[] = SPI_DEVICE;
+    __attribute__((aligned(4))) char recv_buffer[SPI_RECV_BUF_SIZE];
+
+    ext_vfs_init();
+
+    fd = open(device, O_RDWR);
+    if (fd < 0) {
+        printf("Opening device %s for writing failed, errno=%d.\n", device, errno);
+        return;
+    } else {
+        printf("Opening device %s for writing OK, fd=%d.\n", device, fd);
+    }
+
+    cfg.flags = SPI_MODE_0;
+    cfg.cs_pin = SPI_DEVICE_CS_PIN;
+    cfg.sclk_pin = SPI_DEVICE_SCLK_PIN;
+    cfg.mosi_pin = SPI_DEVICE_MOSI_PIN;
+    cfg.miso_pin = SPI_DEVICE_MISO_PIN;
+    cfg.master.clock = SPI_DEVICE_CLOCK;
+    ret = ioctl(fd, SPIIOCSCFG, &cfg);
+    if (ret < 0) {
+        printf("Configure SPI failed, errno=%d.\n", errno);
+        return;
+    }
     ESP_LOGI(TAG, "Waiting for an ISO14443A Card ...");
     while (1)
     {
-        uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0}; // Buffer to store the returned UID
-        uint8_t uid_length;                     // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
-        // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-        // 'uid' will be populated with the UID, and uid_length will indicate
-        // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-        err = pn532_read_passive_target_id(&pn532_io, PN532_BRTY_ISO14443A_106KBPS, uid, &uid_length, 0);
+        spi_ex_msg_t msg;
 
-        if (ESP_OK == err)
-        {
-            // Display some basic information about the card
-            ESP_LOGI(TAG, "\nFound an ISO14443A card");
-            ESP_LOGI(TAG, "UID Length: %d bytes", uid_length);
-            ESP_LOGI(TAG, "UID Value:");
-            ESP_LOG_BUFFER_HEX_LEVEL(TAG, uid, uid_length, ESP_LOG_INFO);
-
-            err = pn532_in_list_passive_target(&pn532_io);
-            if (err != ESP_OK) {
-                ESP_LOGI(TAG, "Failed to inList passive target");
-                continue;
+        memset(recv_buffer, 0, SPI_RECV_BUF_SIZE);
+        msg.rx_buffer = recv_buffer;
+        msg.tx_buffer = NULL;
+        msg.size = SPI_RECV_BUF_SIZE;
+        ret = ioctl(fd, SPIIOCEXCHANGE, &msg);
+        if (ret < 0) {
+            printf("Receive total %d bytes from device failed, errno=%d", SPI_RECV_BUF_SIZE, errno);
+            return;
+        } else {
+            if (msg.size) {
+                printf("Receive total %d bytes from device: %s\n", (int)msg.size, recv_buffer);
             }
-
-            NTAG2XX_MODEL ntag_model = NTAG2XX_UNKNOWN;
-            err = ntag2xx_get_model(&pn532_io, &ntag_model);
-            if (err != ESP_OK)
-                continue;
-
-            int page_max;
-            switch (ntag_model) {
-                case NTAG2XX_NTAG213:
-                    page_max = 45;
-                    ESP_LOGI(TAG, "found NTAG213 target (or maybe NTAG203)");
-                    break;
-
-                case NTAG2XX_NTAG215:
-                    page_max = 135;
-                    ESP_LOGI(TAG, "found NTAG215 target");
-                    break;
-
-                case NTAG2XX_NTAG216:
-                    page_max = 231;
-                    ESP_LOGI(TAG, "found NTAG216 target");
-                    break;
-
-                default:
-                    ESP_LOGI(TAG, "Found unknown NTAG target!");
-                    continue;
-            }
-
-            for(int page=0; page < page_max; page+=4) {
-                uint8_t buf[16];
-                err = ntag2xx_read_page(&pn532_io, page, buf, 16);
-                if (err == ESP_OK) {
-                    ESP_LOG_BUFFER_HEXDUMP(TAG, buf, 16, ESP_LOG_INFO);
-                }
-                else {
-                    ESP_LOGI(TAG, "Failed to read page %d", page);
-                    break;
-                }
-            }
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            // connect to nfc
+            run_nfc(pn532_io, err);
         }
     }
 }
